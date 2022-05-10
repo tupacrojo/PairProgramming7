4f289dbc81bd8bfe98fb2fba9c65722c87772c4f
Tupac Rodriguez <tupacrojo@live.com>
2022-05-10 16:20:15 -0300

    Agregar archivos de proyecto.

---
+++ PairProgramming7/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interpreter.RealWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese numero en romano");
+            string romano = Console.ReadLine();
+            Context context = new Context(romano);
+
+            List<Conversor> tree = new List<Conversor>();
+            tree.Add(new Paramil());
+            tree.Add(new Paracien());
+            tree.Add(new Paradiez());
+            tree.Add(new Parauno());
+
+            foreach (Conversor exp in tree)
+            {
+                exp.Interpret(context);
+            }
+
+            Console.WriteLine("{0} = {1}", romano, context.Output);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Context
+    {
+        string input;
+        int output;
+
+        public Context(string input)
+        {
+            this.input = input;
+        }
+
+        public string Input
+        {
+            get { return input; }
+            set { input = value; }
+        }
+
+        public int Output
+        {
+            get { return output; }
+            set { output = value; }
+        }
+    }
+
+    public abstract class Conversor
+    {
+        public void Interpret(Context context)
+        {
+            if (context.Input.Length == 0)
+                return;
+
+            if (context.Input.StartsWith(Nine()))
+            {
+                context.Output += (9 * Multiplier());
+                context.Input = context.Input.Substring(2);
+            }
+            else if (context.Input.StartsWith(Four()))
+            {
+                context.Output += (4 * Multiplier());
+                context.Input = context.Input.Substring(2);
+            }
+            else if (context.Input.StartsWith(Five()))
+            {
+                context.Output += (5 * Multiplier());
+                context.Input = context.Input.Substring(1);
+            }
+
+            while (context.Input.StartsWith(One()))
+            {
+                context.Output += (1 * Multiplier());
+                context.Input = context.Input.Substring(1);
+            }
+        }
+
+        public abstract string One();
+        public abstract string Four();
+        public abstract string Five();
+        public abstract string Nine();
+        public abstract int Multiplier();
+    }
+
+    public class Paramil : Conversor
+    {
+        public override string One() { return "M"; }
+        public override string Four() { return " "; }
+        public override string Five() { return " "; }
+        public override string Nine() { return " "; }
+        public override int Multiplier() { return 1000; }
+    }
+
+    public class Paracien : Conversor
+    {
+        public override string One() { return "C"; }
+        public override string Four() { return "CD"; }
+        public override string Five() { return "D"; }
+        public override string Nine() { return "CM"; }
+        public override int Multiplier() { return 100; }
+    }
+
+    public class Paradiez : Conversor
+    {
+        public override string One() { return "X"; }
+        public override string Four() { return "XL"; }
+        public override string Five() { return "L"; }
+        public override string Nine() { return "XC"; }
+        public override int Multiplier() { return 10; }
+    }
+
+    public class Parauno : Conversor
+    {
+        public override string One() { return "I"; }
+        public override string Four() { return "IV"; }
+        public override string Five() { return "V"; }
+        public override string Nine() { return "IX"; }
+        public override int Multiplier() { return 1; }
+    }
+}
